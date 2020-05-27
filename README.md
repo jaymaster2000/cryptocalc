6f310f1e6ce915038c9ea2ab8212eb5a0c894997
jaymaster2000 <jay.davis@21ltr.com>
2020-05-27 10:21:22 +1000

    Add script, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # cryptocalc
 A Python3 script for encrypting and decrypting data using AES-GCM and ascii hex strings. Ideally for use on an air-gapped Raspberry Pi using the inbuilt hardware random number generator.
+
+## Example
+
+### Encryption
+
+```
+$ ./cryptocalc.py -e
+Enter plaintext: super secret data 
+Enter passphrase: correct horse battery staple
+=================================================================================
+Encrypted:
+Salt: 		b1042b9c1a970e03
+IV: 		9edba651959077ea
+		d9f665a6
+Ciphertext: 	9be071a2e40b3fc1
+		20c41e9b30f5126f
+		1f288c5b862c1517
+		fdfbbd0a4f125956
+		8d
+=================================================================================
+```
+
+### Decryption
+
+```
+$ ./cryptocalc.py -d
+Enter salt: b1042b9c1a970e03
+Enter iv: 9edba651959077ead9f665a6
+Enter ciphertext: 9be071a2e40b3fc120c41e9b30f5126f1f288c5b862c1517fdfbbd0a4f1259568d
+Enter passphrase: correct horse battery staple
+=================================================================================
+Decrypted:
+super secret data
+=================================================================================
+```
---
+++ cryptocalc.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+"""A python program to encrypt and decrypt data to bytes
+   for recording onto cold mediums."""
+
+import sys
+import hashlib
+#import os
+from binascii import hexlify, unhexlify
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from rdrand import RdRandom
+
+BYTE_DISPLAY_SIZE = 8
+RANDOM_SOURCE = RdRandom()
+
+def derive_key(passphrase: str, salt: bytes = None) -> [str, bytes]:
+    """converts a passphrase into a key by using or generating a salt."""
+    if salt is None:
+        #salt = os.urandom(8)
+        salt = RANDOM_SOURCE.getrandbytes(8)
+    return hashlib.pbkdf2_hmac("sha256", passphrase.encode("utf8"), salt, 1000), salt
+
+
+def encrypt(passphrase: str, plaintext: str) -> str:
+    """encrypts plaintext data using the user supplied passphrase."""
+    key, salt = derive_key(passphrase)
+    aes = AESGCM(key)
+    #initialization_vector = os.urandom(12)
+    initialization_vector = RANDOM_SOURCE.getrandbytes(12)
+    iv_hexstring = hexlify(initialization_vector).decode("utf8")
+    iv_chunks = [iv_hexstring[a: a + BYTE_DISPLAY_SIZE * 2] for a in range(0, len(iv_hexstring), \
+            BYTE_DISPLAY_SIZE * 2)]
+    plaintext = plaintext.encode("utf8")
+    ciphertext = aes.encrypt(initialization_vector, plaintext, None)
+    ciphertext_hexstring = hexlify(ciphertext).decode("utf8")
+    ciphertext_chunks = [ciphertext_hexstring[i: i + BYTE_DISPLAY_SIZE * 2] for i in \
+            range(0, len(ciphertext_hexstring), BYTE_DISPLAY_SIZE * 2)]
+    print('=================================================================================')
+    print('Encrypted:')
+    print('Salt: ' + '\t\t' + hexlify(salt).decode("utf8"))
+    print('IV: ' + '\t\t' + iv_chunks.pop(0))
+    if iv_chunks:
+        for chunks in iv_chunks:
+            print('\t' + '\t' + chunks)
+    print('Ciphertext: ' + '\t' + ciphertext_chunks.pop(0))
+    if ciphertext_chunks:
+        for chunks in ciphertext_chunks:
+            print('\t' + '\t' + chunks)
+    print('=================================================================================')
+
+
+def decrypt(passphrase: str, ciphertext: str) -> str:
+    """decrypts ciphertext data using the user supplied passphrase and the salt and iv."""
+    try:
+        salt, initialization_vector, ciphertext = map(unhexlify, ciphertext.split("-"))
+        key, _ = derive_key(passphrase, salt)
+        aes = AESGCM(key)
+        plaintext = aes.decrypt(initialization_vector, ciphertext, None)
+        print('=================================================================================')
+        print('Decrypted:')
+        print(plaintext.decode("utf8"))
+        print('=================================================================================')
+    except:
+        print(sys.exc_info())
+        print('Decryption failed, wrong key or wrong bytes?')
+
+def usage():
+    """prints usage information."""
+    print('usage: aes.py [-e] [-d]')
+    print('')
+    print(' -e encrypt data')
+    print(' -d decrypt data')
+    print('')
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '-e':
+            USER_PLAINTEXT = input("Enter plaintext: ")
+            USER_PASSPHRASE = input("Enter passphrase: ")
+            encrypt(USER_PASSPHRASE, USER_PLAINTEXT)
+        elif sys.argv[1] == '-d':
+            USER_SALT = input("Enter salt: ")
+            USER_IV = input("Enter iv: ")
+            USER_CIPHERTEXT = input("Enter ciphertext: ")
+            USER_PASSPHRASE = input("Enter passphrase: ")
+            CONCAT_CIPHERTEXT = USER_SALT + '-' + USER_IV + '-' + USER_CIPHERTEXT
+            decrypt(USER_PASSPHRASE, CONCAT_CIPHERTEXT)
+        else:
+            usage()
+    else:
+        usage()
